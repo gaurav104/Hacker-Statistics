da8fe37296c6ba68983ec2c2960dbfd73ca77d58
Gaurav Patel <gouravpatel8@gmail.com>
2017-05-11 01:02:29 +0530

    Add files via upload

---
+++ Hacker Statistics.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+np.random.seed(123)
+all_walks = []
+for i in range(500):
+        random_walk=[0]
+        for i in range(100):
+                step = random_walk[-1]
+                dice = np.random.randint(1,7)
+                if dice<=2:
+                        step = max(0,step-1)
+                elif dice<=5:
+                        step = step + 1
+                else:
+                        step = step + np.random.randint(1,7)
+                random_walk.append(step)
+        all_walks.append(random_walk)	
+
+np_aw = np.array(all_walks)
+np_aw_t = np.transpose(np_aw)
+
+
+ends = np_aw_t[-1,:]
+#plt.hist(ends) to form the distribution
+#plt.show() to show the distribution
+print(np.mean(ends>60))
